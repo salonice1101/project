24ecac8cef32ad872f5ea2f9a4d0acf1999815f1
salonice1101 <salonice1101@gmail.com>
2018-04-03 23:51:37 +0530

    commit_3

--- project.c
+++ project.c
@@ -98,4 +98,53 @@ void optimal()
 {
 	int count[10],i,j,k,fault,f,flag,temp,current,c,dist,max,m,cnt,p,x;
 	fault=0;
-
+dist=0;
+	k=0;
+	printf("\nEnter frame size:");
+	scanf("%d",&f);
+	//initilizing distance and frame array	
+	for(i=0;i<f;i++)
+	{
+		count[i]=0;
+		fr[i]=-1;
+	}
+	for(i=0;i<n;i++)
+	{
+		flag=0;
+		temp=pg[i];
+		for(j=0;j<f;j++)			
+		{
+			if(temp==fr[j])
+			{
+				flag=1;
+				break;
+			}
+		}
+		if((flag==0)&&(k<f))
+		{
+			fault++;
+			fr[k]=temp;
+			k++;
+		}
+		else if((flag==0)&&(k==f))
+		{
+			fault++;			
+			for(cnt=0;cnt<f;cnt++)			
+			{		
+				current=fr[cnt];
+				for(c=i;c<n;c++)
+				{
+					if(current!=pg[c])
+						count[cnt]++;
+					else
+						break;
+				}
+			}
+			max=0;
+			for(m=0;m<f;m++)
+			{
+				if(count[m]>max)
+				{
+					max=count[m];
+					p=m;
+				}
